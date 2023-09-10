74203b74c7bdecfa44e73917311556d0e1651970
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2023-09-11 03:43:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,6 +29,4 @@ sed -i '/set wireless.default_radio0.ssid=RT-ACRH17/a\ set wireless.default_radi
 
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 
-sed -i 's/"CPU 性能优化调节"/"超频"/g' feeds/luci/applications/luci-app-cpufreq/po/zh-cn/cpufreq.po
-
 git clone --branch master https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/lean/luci-app-unblockneteasemusic
