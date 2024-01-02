b08771d6cc3a88ab888a4a78c726a2d69e984583
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2024-01-02 16:14:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,6 @@
 
 # Add a feed source
 
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
