e3ac1260b0d4883acb0298005c21702ad5de27ce
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2025-06-04 21:34:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' package/lean/default-settings/files/zzz-default-settings
 
-sed -i "s/OpenWrt /OPWRT/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/LEDE /OPWRT/g" package/lean/default-settings/files/zzz-default-settings
 
 sed -i 's/R25.5.25/SVIP/g' package/lean/default-settings/files/zzz-default-settings
 
